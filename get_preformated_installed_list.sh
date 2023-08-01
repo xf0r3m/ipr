1563abaf142c0229d362c606a978c2debd921d76
xf0r3m <jakubstasinski@protonmail.com>
2023-08-01 16:07:13 +0200

    Dodanie dwóch pakietów: 'firefox-esr' oraz 'xfce4-notes-plugin'. Rozwiązanie problemu z wartością 'N/I' przy 'xfce4-notes-plugin' dla wersji stable - BT #100

--- get_preformated_installed_list.sh
+++ get_preformated_installed_list.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 apt-get update > /dev/null 2>&1;
-apt list | grep 'installed' | awk '{printf $1" "$2"\n"}' | sed -e "s/\/${1},.*\ / /g" -e "s/\/${1}\ / /g"
+apt list | grep 'installed' | awk '{printf $1" "$2"\n"}' | sed -e "s/\/${1},.*\ / /g" -e "s/\/${1}\ / /g" -e "s/\/now\ / /g"
