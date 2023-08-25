a2425b68150e0a54d0a22ce2e032dbf88cc26695
xf0r3m <jakubstasinski@protonmail.com>
2023-08-25 21:43:33 +0200

    Brak jądra - BT #105

--- get_preformated_installed_list.sh
+++ get_preformated_installed_list.sh
@@ -2,4 +2,4 @@
 
 apt-get update > /dev/null 2>&1;
 #apt list | grep 'installed' | awk '{printf $1" "$2"\n"}' | sed -e "s/\/${1},.*\ / /g" -e "s/\/${1}\ / /g" -e "s/\/now\ / /g"
-apt list | grep 'installed' | awk '{printf $1" "$2"\n"}' | sed -e "s/\/${1},.*\ / /g" -e "s/\/${1}\ / /g" -e "s/\/${1}-security\ / /g";
+apt list | grep 'installed' | awk '{printf $1" "$2"\n"}' | sed -e "s/\/${1},.*\ / /g" -e "s/\/${1}\ / /g" -e "s/\/now\ / /g" -e "s/\/${1}-security,now\ / /g";
