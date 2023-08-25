b555d40146925a60a33fbdcd6424034e89438a1c
xf0r3m <jakubstasinski@protonmail.com>
2023-08-25 21:07:11 +0200

    Próba rozwiązania problemu BT #105 - Brak jądra

--- get_preformated_installed_list.sh
+++ get_preformated_installed_list.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 apt-get update > /dev/null 2>&1;
-apt list | grep 'installed' | awk '{printf $1" "$2"\n"}' | sed -e "s/\/${1},.*\ / /g" -e "s/\/${1}\ / /g" -e "s/\/now\ / /g"
+#apt list | grep 'installed' | awk '{printf $1" "$2"\n"}' | sed -e "s/\/${1},.*\ / /g" -e "s/\/${1}\ / /g" -e "s/\/now\ / /g"
+apt list | grep 'installed' | awk '{printf $1" "$2"\n"}' | sed -e "s/\/${1},.*\ / /g" -e "s/\/${1}\ / /g" -e "s/\/${1}-security\ / /g";
